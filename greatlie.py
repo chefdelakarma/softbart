48cba32d67cb22aa9491e0f6f728a914978ec685
chefsoftbart <159172026+chefsoftbart@users.noreply.github.com>
2024-02-17 18:57:53 +0100

    Update greatlie.py

--- greatlie.py
+++ greatlie.py
@@ -3,30 +3,24 @@
 from datetime import datetime
 
 class greatlie:
-	def __init__(self, number_choices, wanted_choice):
-		self.number_choices = number_choices
+	def __init__(self, wanted_choice):
 		self.list_lies = []
 		self.list_answers = []
 		self.list_lies.append(wanted_choice)
-		self.nlies=1
-		self.nanswers=0
 		self.date_lies = []
 		self.date_answers = []
 		self.date_lies.append(datetime.now())
 	def setlie(self,wanted_choice):
 		self.list_lies.append(wanted_choice)
-		self.nlies+=1
 		self.date_lies.append(datetime.now())
 	def setanswer(self, answer):
 		self.list_answers.append(answer)
-		self.nanswers+=1
 		self.date_answers.append(datetime.now())
 	def islie():
-		if self.list_lies[self.nlies-1] != self.list_answers[self.nanswers-1]:
-			return True
-		else:
+		if self.list_lies[-1] += self.list_answers[-1]:
 			return False
+		return True
 	def getanswer():
-		return self.list_lies[self.nlies - 1]
+		return self.list_lies[ 1]
 		
-object_greatlie = greatlie(3,1)
+object_greatlie = greatlie(3)
