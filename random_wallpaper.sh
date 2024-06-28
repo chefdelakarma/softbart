1480d23f12d51c55c3b440a903a791f35d8fba24
Le chef de karma <159172026+chefsoftbart@users.noreply.github.com>
2024-06-28 14:45:28 +0200

    Add files via upload

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-
-mode=${mode:-fill}
-output=${output:-\*}
+export WAYLAND_DISPLAY=wayland-1
+export SWAYSOCK=$(ls /run/user/$(id -u)/sway-ipc.*.sock)
+export I3SOCK=$(ls /run/user/$(id -u)/sway-ipc.*.sock)
+export XDG_SEAT=seat0
+export XDG_VTNR=1
+export XDG_SESSION_ID=1
+export XDG_RUNTIME_DIR=/run/user/1000
 
 # Directory containing wallpapers
-WALLPAPER_DIR="/usr/share/wallpapers/distrotube"
+WALLPAPER_DIR="/usr/share/wallpapers"
 
 # Select a random wallpaper
-RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f -name "*.jpg" | shuf -n 1)
 
 [[ $(pgrep ^swaybg) ]] && pkill ^swaybg
-
-# Set the wallpaper
-swaybg -m $mode -o "$output" -i $RANDOM_WALLPAPER &
+swaybg -m fill -o "*" -i "${RANDOM_WALLPAPER}" &
