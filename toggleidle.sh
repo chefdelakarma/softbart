e17593e651d7d94107c10f69817d7c9aba06d752
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-02-09 16:55:30 +0100

    Add files via upload

--- toggleidle.sh
+++ toggleidle.sh
@@ -6,14 +6,13 @@
 expire=${expire:-5000}
 if [[ $(pgrep ^swayidle) ]]; then
 	notify-send -t $expire "swayidle off"
-	pkill ^swayidle
+	pkill -f swayidle
 else
 	notify-send -t $expire "swayidle on"
 	
-pkill -f ^swayidle
-mapfile -t screens_on < <(swaymsg -r -t get_outputs | jq -r '.[] | select(.power == true) | .name')
-	
-swayidle -w \
-    timeout 300 "$(for screen in "${screens_on[@]}"; do echo "swaymsg output $screen power off"; done)" \
-    resume "$(for screen in "${screens_on[@]}"; do echo "swaymsg output $screen power on"; done)" &
+	pkill -f swayidle
+	mapfile -t screens_on < <(swaymsg -r -t get_outputs | jq -r '.[] | select(.power == true) | .name')
+	swayidle -w \
+	    timeout 300 "$(for screen in "${screens_on[@]}"; do echo "swaymsg output $screen power off"; done)" \
+	    resume "$(for screen in "${screens_on[@]}"; do echo "swaymsg output $screen power on"; done)" &
 fi
--- togglescreen.sh
+++ togglescreen.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-#!/bin/bash
 # AUTHOR:			BJ Veurink
 # NAME: 			toggle swayscreen
 # LICENSE:		GNU GPLv2
 
-#screen1="eDP-1"
-#screen2="HDMI-A-1"
 screen=${1:-"eDP-1"}
 expire=${expire:-5000}
 if swaymsg -r -t get_outputs | jq -e --arg screen "$screen" '.[] | select(.name==$screen) | .power' > /dev/null; then
@@ -15,7 +12,7 @@ else
     swaymsg output ${screen} power on
 fi
 
-pkill -f ^swayidle
+pkill -f swayidle
 mapfile -t screens_on < <(swaymsg -r -t get_outputs | jq -r '.[] | select(.power == true) | .name')
 
 swayidle -w \
