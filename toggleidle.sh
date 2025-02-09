2566f9e74df2ec302eb27e9434dac9d014f4bbcd
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-02-09 15:07:28 +0100

    Add files via upload

--- toggleidle.sh
+++ toggleidle.sh
@@ -9,6 +9,10 @@ if [[ $(pgrep ^swayidle) ]]; then
 	pkill ^swayidle
 else
 	notify-send -t $expire "swayidle on"
+	
+pkill -f ^swayidle
+mapfile -t screens_on < <(swaymsg -r -t get_outputs | jq -r '.[] | select(.power == true) | .name')
+	
 swayidle -w \
     timeout 300 "$(for screen in "${screens_on[@]}"; do echo "swaymsg output $screen power off"; done)" \
     resume "$(for screen in "${screens_on[@]}"; do echo "swaymsg output $screen power on"; done)" &
