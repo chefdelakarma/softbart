1112a3dbfafff87403282bcbf3dee3d62c29d941
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-02-21 15:07:20 +0100

    Add files via upload

--- bluetoothconn.sh
+++ bluetoothconn.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Vervang dit met het MAC-adres van je Bluetooth-apparaat
-DEVICE_MAC="${1}"
+DEVICE_MAC="xx:xx:xx:xx:xx:xx"
 
 # Zet Bluetooth aan
 rfkill unblock bluetooth
 
 # Start bluetoothctl en voer commando's uit
-bluetoothctl << EOF
+if bluetoothctl << EOF
 power on
 agent on
 default-agent
 connect $DEVICE_MAC
 EOF
-
-echo "Verbonden met $DEVICE_MAC"
-
+then
+	notify-send "Verbonden met $DEVICE_MAC"
+else
+	notify-send "bluetooth connect error"
+fi
