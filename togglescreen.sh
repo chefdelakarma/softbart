66763c79bd68cacdd40c826fb62b13f9a48a7ebb
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-02-21 15:06:26 +0100

    Add files via upload

--- togglescreen.sh
+++ togglescreen.sh
@@ -2,16 +2,17 @@
 # AUTHOR:			BJ Veurink
 # NAME: 			toggle swayscreen
 # LICENSE:		GNU GPLv2
-
-screen=${1:-"eDP-1"}
+args=("$@")
+#screen=${1:-"eDP-1"}
 expire=${expire:-5000}
-if swaymsg -r -t get_outputs | jq -e --arg screen "$screen" '.[] | select(.name==$screen) | .power' > /dev/null; then
-    swaymsg output ${screen} power off
-    notify-send -t $expire "screen ${screen} off"
-else
-    swaymsg output ${screen} power on
-fi
-
+for screen in ${args[@]}; do
+	if swaymsg -r -t get_outputs | jq -e --arg screen "$screen" '.[] | select(.name==$screen) | .power' > /dev/null; then
+	    swaymsg output ${screen} power off
+	    notify-send -t $expire "screen ${screen} off"
+	else
+ 	   swaymsg output ${screen} power on
+	fi
+done
 pkill -f swayidle
 mapfile -t screens_on < <(swaymsg -r -t get_outputs | jq -r '.[] | select(.power == true) | .name')
 
