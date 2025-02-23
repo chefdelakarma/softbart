219e0027366fc55fb62a5e4de4d6692b326b6f44
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-02-23 17:27:21 +0100

    Add files via upload

--- bluetoothconn.sh
+++ bluetoothconn.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
+
 # Vervang dit met het MAC-adres van je Bluetooth-apparaat
-DEVICE_MAC="xx:xx:xx:xx:xx:xx"
+DEVICE_MAC=""
+
+if bluetoothctl info $DEVICE_MAC | grep -q "Connected: yes"; then
+	bluetoothctl disconnect $DEVICE_MAC
+	notify-send "Disconnect $DEVICE_MAC"
+	exit 0
+fi
 
 # Zet Bluetooth aan
 rfkill unblock bluetooth
-sleep 2
+sleep 5
 
 # Start bluetoothctl en voer commando's uit
-if bluetoothctl << EOF
+bluetoothctl << EOF
 power on
 agent on
 default-agent
 trust $DEVICE_MAC
 pair $DEVICE_MAC
-connect $DEVICE_MAC
 EOF
-then
-	notify-send "Verbonden met $DEVICE_MAC"
+sleep 5
+bluetoothctl connect $DEVICE_MAC
+
+
+if bluetoothctl info $DEVICE_MAC | grep -q "Connected: yes"; then
+	notify-send "Connected $DEVICE_MAC" 
 else
-	notify-send "bluetooth connect error"
+	notify-send "error bluetooth"
 fi
-
-# Controleer of de verbinding succesvol is, zo niet, probeer opnieuw
-for i in {1..3}; do
-    if bluetoothctl info $DEVICE_MAC | grep -q "Connected: yes"; then
-        echo "Succesvol verbonden met $DEVICE_MAC"
-        exit 0
-    fi
-    echo "Verbinding mislukt, opnieuw proberen ($i/3) ..."
-    sleep 2
-    bluetoothctl connect $DEVICE_MAC
-done
-
-echo "Kon geen verbinding maken met $DEVICE_MAC."
