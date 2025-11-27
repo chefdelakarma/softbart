e13a4b749c290989cb5649cf9cdffcb06f9acd94
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-11-27 13:05:26 +0100

    Update pdfwithocr.sh

--- pdfwithocr.sh
+++ pdfwithocr.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-dir=${1:-~/recepten.ocr}
+pdffile=${1:-input.pdf}
+dir=${2:-~/recepten.ocr}
 [[ -d ${dir} ]] || mkdir -pv ${dir}
 language=nld
+[[ -d source ]] || mkdir -pv source
+pdftk $pdffile burst
+mv $pdffile source/
 
 for i in *.pdf
 do
